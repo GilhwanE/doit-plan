62b250dacb37c25b7c085519344d46d8dfe56bc3
GilHwan <dyghks123@gmail.com>
2022-05-12 20:04:51 +0900

    🚧  TypeDefine App, handleAdd

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import InputFelid from './components/inputFelid';
+import { Todo } from './modle';
 
 const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>('');
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="App">
       <header className="header">오늘의 할일</header>
-      <InputFelid />
+      <InputFelid todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
     </div>
   );
 };
