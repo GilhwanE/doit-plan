8f73e40b07b009dc9ae4f6468db73da420e6de52
GilHwan <dyghks123@gmail.com>
2022-05-15 19:09:02 +0900

    🚧  Using Droppable provided, snapshot

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,25 +1,67 @@
 import React from 'react';
+import { Droppable } from 'react-beautiful-dnd';
 import { Todo } from '../modle';
-import SingleTodo from './SingleTodo';
+import SingleTodo from './singleTodo';
 
 import './style.css';
 
 interface Props {
   todos: Todo[];
   setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+  completeTodo: Todo[];
+  setCompleteTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
 
-const ToDoList: React.FC<Props> = ({ todos, setTodos }) => {
+const ToDoList: React.FC<Props> = ({
+  todos,
+  setTodos,
+  completeTodo,
+  setCompleteTodos,
+}) => {
   return (
-    <div className="todos">
-      {todos.map((todo) => (
-        <SingleTodo
-          todo={todo}
-          key={todo.id}
-          todos={todos}
-          setTodos={setTodos}
-        />
-      ))}
+    <div className="container">
+      <Droppable droppableId="TodosList">
+        {(provided, snapshot) => (
+          <div
+            className={`todos ${snapshot.isDraggingOver} ? 'dragactive' : ''`}
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+          >
+            <h1 className="todos__heading">해야할일</h1>
+            {todos.map((todo, index) => (
+              <SingleTodo
+                index={index}
+                todo={todo}
+                todos={todos}
+                key={todo.id}
+                setTodos={setTodos}
+              />
+            ))}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+      <Droppable droppableId="TodosRemove">
+        {(provided, snapshot) => (
+          <div
+            className={`todos remove ${snapshot.isDraggingOver} ? 'dragcomplete' : ''`}
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+          >
+            <h1 className="todos__heading">완료한 일</h1>
+            {completeTodo.map((todo, index) => (
+              <SingleTodo
+                index={index}
+                todo={todo}
+                todos={completeTodo}
+                key={todo.id}
+                setTodos={setCompleteTodos}
+              />
+            ))}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
     </div>
   );
 };
